12490a3787f0d0d9469908c7716627451c6dc3c7
sberk <lp.panda@proton.me>
2026-05-26 13:22:45 +0200

    Update appwrite.js

--- js/appwrite.js
+++ js/appwrite.js
@@ -1,8 +1,13 @@
 import {
 Client,
-Account
+Account,
+Databases
 }
 from "https://cdn.jsdelivr.net/npm/appwrite@15.0.0/+esm";
+
+/* =========================
+APPWRITE CLIENT
+========================= */
 
 const client = new Client();
 
@@ -11,8 +16,37 @@ client
 "https://cloud.appwrite.io/v1"
 )
 .setProject(
-"DEINE_PROJECT_ID"
+"6a156f3c00292515cea9"
 );
+
+/* =========================
+SERVICES
+========================= */
 
 export const account =
 new Account(client);
+
+export const databases =
+new Databases(client);
+
+/* =========================
+DATABASE IDS
+========================= */
+
+/*
+HIER DEINE IDs EINTRAGEN
+*/
+
+export const DATABASE_ID =
+"6a15726400182ebba703";
+
+export const NEWS_TABLE_ID =
+"news";
+
+/* =========================
+TEST CONNECTION
+========================= */
+
+console.log(
+"✅ Appwrite verbunden"
+);
