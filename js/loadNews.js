b6305f724690c482609984a4fef700bad6915cce
sberk <lp.panda@proton.me>
2026-05-26 13:55:00 +0200

    Update loadNews.js

--- js/loadNews.js
+++ js/loadNews.js
@@ -1,26 +1,84 @@
 import {
-databases
+databases,
+DATABASE_ID,
+NEWS_TABLE_ID
 }
 from "./appwrite.js";
 
+import {
+Query
+}
+from "https://cdn.jsdelivr.net/npm/appwrite@15.0.0/+esm";
+
+const newsContainer =
+document.getElementById(
+"newsContainer"
+);
+
+async function loadNews(){
+
+try{
+
 const response =
 await databases.listDocuments(
-"6a15726400182ebba703",
-"news"
+
+DATABASE_ID,
+NEWS_TABLE_ID,
+
+[
+Query.orderDesc("$createdAt")
+]
+
 );
+
+newsContainer.innerHTML = "";
 
 response.documents.forEach(doc=>{
 
-document.getElementById(
-"newsContainer"
-).innerHTML += `
+newsContainer.innerHTML += `
 
-<div class="card">
+<div class="news-card">
 
+<div class="news-top">
+
+<span class="news-author">
+${doc.author}
+</span>
+
+<span class="news-date">
+${doc.date}
+</span>
+
+</div>
+
+<h2 class="news-heading">
+${doc.title}
+</h2>
+
+<p class="news-content">
 ${doc.content}
+</p>
 
 </div>
 
 `;
 
 });
+
+}catch(error){
+
+console.error(error);
+
+newsContainer.innerHTML =
+
+`
+<p>
+Fehler beim Laden der News
+</p>
+`;
+
+}
+
+}
+
+loadNews();
