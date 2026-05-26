00217062f0dd1b10223491d4d744ed6c7953bfd9
sberk <lp.panda@proton.me>
2026-05-26 11:35:57 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,53 @@
+function copyIP(){
+
+navigator.clipboard.writeText(
+"play.ultragames.net"
+);
+
+alert("Server-IP kopiert!");
+
+}
+
+/* SERVER STATUS */
+
+async function loadServerStatus(){
+
+try{
+
+const response = await fetch(
+"https://api.mcsrvstat.us/2/play.ultragames.net"
+);
+
+const data = await response.json();
+
+if(data.online){
+
+document.getElementById(
+"serverStatus"
+).innerHTML =
+
+`🟢 Online • ${data.players.online} Spieler`;
+
+}else{
+
+document.getElementById(
+"serverStatus"
+).innerHTML =
+
+`🔴 Offline`;
+
+}
+
+}catch(err){
+
+document.getElementById(
+"serverStatus"
+).innerHTML =
+
+`⚠ Fehler beim Laden`;
+
+}
+
+}
+
+loadServerStatus();
